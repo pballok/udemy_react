93924810af91f1513db312f2d83ac1bf18cc5e8d
Peter Ballok <pballok@gmail.com>
2020-05-08 08:38:00 -0400

    Output "Hi there!" message

---
+++ jsx/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = function() {
+  return <div>Hi there!</div>;
+};
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
