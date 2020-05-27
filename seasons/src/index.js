c67786499311ec71883f771f95bf38341760dbaa
Peter Ballok <pballok@gmail.com>
2020-05-27 09:52:32 -0400

    Use class instead of function-based component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return <div>Hi there!</div>;
-};
+class App extends React.Component {
+  render() {
+    return <div>Latitude:</div>;
+  }
+}
 
 ReactDOM.render(
   <App />,
