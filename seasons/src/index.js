31dba3968b8e98c15098aa28f6323e8721e5e852
Peter Ballok <pballok@gmail.com>
2020-05-30 16:27:19 -0400

    Display latitude

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,8 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  state = {
+    lat: null,
+    errorMessage: ''
+  };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
   render() {
-    return <div>Latitude:</div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading...</div>;
   }
 }
 
